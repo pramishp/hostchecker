0a4377829a4d35e41be921a9da23948d040b6dfc
himalg7 <thapahimal777@gmail.com>
2020-01-06 22:39:29 +0545

    broken texts added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='checkhost',
-    version='0.4',
+    version='0.5',
     packages= ['checkhost'],
     description='host checker ',
     url='https://github.com/VeshRaazThapa/hostcheckermain.git'
